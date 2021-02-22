84d3b693b9dbca36e79727eba31a151822e690b2
Minh SB <minh@skoolbo.com>
2021-02-22 21:02:18 +1100

    promotion service and tests

---
+++ src/services/promotion.js
@@ -0,0 +1,68 @@
+var Catalogue = require("../services/catalogue");
+var PricingRule = require("../models/pricingRule");
+
+const pricingRules = [
+  new PricingRule({
+    name: "3 for 2 atv",
+    description: "If you buy 3 Apple TVs, you will pay the price of 2 only",
+    condition: (cart) => !!cart && cart.atv && cart.atv.quantity >= 3,
+    action: (cart) => {
+      const bundles = Math.floor(cart.atv.quantity / 3);
+      cart.atv.discount = bundles * Catalogue.getPrice("atv");
+      return cart.atv.discount;
+    },
+  }),
+  new PricingRule({
+    name: ">4 ipd $499.99 each",
+    description:
+      "The brand new Super iPad will have a bulk discounted applied, where the price will drop to $499.99 each, if someone buys more than 4",
+    condition: (cart) => !!cart && cart.ipd && cart.ipd.quantity > 4,
+    action: (cart) => {
+      const bundles = cart.ipd.quantity > 4 ? cart.ipd.quantity : 0;
+      cart.ipd.discount = bundles * (Catalogue.getPrice("ipd") - 499.99);
+      return cart.ipd.discount;
+    },
+  }),
+  new PricingRule({
+    name: "mbp free vga",
+    description:
+      "We will bundle in a free VGA adapter free of charge with every MacBook Pro sold.",
+    condition: (cart) =>
+      !!cart &&
+      cart.mbp &&
+      cart.mbp.quantity >= 1 &&
+      cart.vga &&
+      cart.vga.quantity,
+    action: (cart) => {
+      const bundles = Math.min(
+        cart.mbp ? cart.mbp.quantity : 0,
+        cart.vga ? cart.vga.quantity : 0
+      );
+      cart.vga.discount = bundles * Catalogue.getPrice("vga");
+      return cart.vga.discount;
+    },
+  }),
+];
+
+/**
+ * A service to manage pricing Rules
+ */
+const Promotion = {
+  // get 1 pricing rule by index
+  getPricingRule: function (index) {
+    if (!(index >= 0 && index < pricingRules.length)) return null;
+    return pricingRules[index];
+  },
+
+  // Get 1 pricing rule by name
+  getPricingRuleByName: function (name) {
+    return pricingRules.find((rule) => rule.name === name);
+  },
+
+  // Return all pricing rules
+  getPricingRules: function () {
+    return pricingRules;
+  },
+};
+
+module.exports = Promotion;
---
+++ src/services/tests/promotion.test.js
@@ -0,0 +1,126 @@
+const PricingRule = require("../../models/pricingRule");
+const Catalogue = require("../catalogue");
+const Promotion = require("../promotion");
+
+// test getPricingRules
+test("getPricingRules should return an array of PricingRule", () => {
+  const pricingRules = Promotion.getPricingRules();
+  expect(Array.isArray(pricingRules)).toBe(true);
+  expect(pricingRules).toHaveLength(3);
+  pricingRules.forEach((rule) => {
+    expect(rule instanceof PricingRule).toBe(true);
+  });
+});
+
+test("The pricingRules data is correct", () => {
+  expect(Promotion.getPricingRules()).toMatchSnapshot();
+});
+
+// test getPricingRule
+test("getPricingRule with proper index should work", () => {
+  {
+    const pricingRule = Promotion.getPricingRule(0);
+    expect(pricingRule instanceof PricingRule).toBe(true);
+  }
+  {
+    const pricingRule = Promotion.getPricingRule(1);
+    expect(pricingRule instanceof PricingRule).toBe(true);
+  }
+  {
+    const pricingRule = Promotion.getPricingRule(2);
+    expect(pricingRule instanceof PricingRule).toBe(true);
+  }
+});
+
+test("getPricingRule with negative index should return null", () => {
+  const pricingRule = Promotion.getPricingRule(-1);
+  expect(pricingRule).toBe(null);
+});
+
+test("getPricingRule with out of boud index should return null", () => {
+  const pricingRule = Promotion.getPricingRule(
+    Promotion.getPricingRules().length
+  );
+  expect(pricingRule).toBe(null);
+});
+
+// test getPricingRuleByName
+test("getPricingRuleByName with wrong sku should return null", () => {
+  const pricingRule = Promotion.getPricingRuleByName("random string");
+  expect(pricingRule).toBe(undefined);
+});
+
+test("getPricingRuleByName with correct sku should work", () => {
+  const pricingRule = Promotion.getPricingRuleByName("3 for 2 atv");
+  expect(pricingRule.name).toBe("3 for 2 atv");
+});
+
+// Mock test each Pricing Rule
+test("'3 for 2 atv' pricingRule should give 1 free for every 3 atv in cart", () => {
+  const pricingRule = Promotion.getPricingRuleByName("3 for 2 atv");
+  const itemPrice = Catalogue.getPrice("atv");
+  {
+    const cart = { atv: { quantity: 2 } };
+    expect(pricingRule.condition(cart)).toBeFalsy();
+    expect(pricingRule.action(cart)).toBe(0);
+    expect(cart.atv.discount).toBe(0);
+  }
+  {
+    const cart = { atv: { quantity: 3 } };
+    expect(pricingRule.condition(cart)).toBeTruthy();
+    expect(pricingRule.action(cart)).toBeGreaterThan(0);
+    expect(cart.atv.discount).toBe(itemPrice);
+  }
+  {
+    const cart = { atv: { quantity: 10 } };
+    expect(pricingRule.condition(cart)).toBeTruthy();
+    expect(pricingRule.action(cart)).toBeGreaterThan(0);
+    expect(cart.atv.discount).toBe(3 * itemPrice);
+  }
+});
+
+test("'>4 ipd $499.99 each' pricingRule should reduce ipd price to $499.99 each", () => {
+  const pricingRule = Promotion.getPricingRuleByName(">4 ipd $499.99 each");
+  const itemPrice = Catalogue.getPrice("ipd");
+  {
+    const cart = { ipd: { quantity: 4 } };
+    expect(pricingRule.condition(cart)).toBeFalsy();
+    expect(pricingRule.action(cart)).toBe(0);
+    expect(cart.ipd.discount).toBe(0);
+  }
+  {
+    const cart = { ipd: { quantity: 5 } };
+    expect(pricingRule.condition(cart)).toBeTruthy();
+    expect(pricingRule.action(cart)).toBeGreaterThan(0);
+    expect(cart.ipd.discount).toBe(5 * (itemPrice - 499.99));
+  }
+  {
+    const cart = { ipd: { quantity: 8 } };
+    expect(pricingRule.condition(cart)).toBeTruthy();
+    expect(pricingRule.action(cart)).toBeGreaterThan(0);
+    expect(cart.ipd.discount).toBe(8 * (itemPrice - 499.99));
+  }
+});
+
+test("'mbp free vga' should free 1 vga for every mbp", () => {
+  const pricingRule = Promotion.getPricingRuleByName("mbp free vga");
+  const itemPrice = Catalogue.getPrice("vga");
+  {
+    const cart = { vga: { quantity: 2 } };
+    expect(pricingRule.condition(cart)).toBeFalsy();
+    expect(pricingRule.action(cart)).toBe(0);
+    expect(cart.vga.discount).toBe(0);
+  }
+  {
+    const cart = { vga: { quantity: 5 }, mbp: { quantity: 3 } };
+    expect(pricingRule.condition(cart)).toBeTruthy();
+    expect(pricingRule.action(cart)).toBeGreaterThan(0);
+    expect(cart.vga.discount).toBe(3 * itemPrice);
+  }
+  {
+    const cart = { vga: { quantity: 2 }, mbp: { quantity: 9 } };
+    expect(pricingRule.condition(cart)).toBeTruthy();
+    expect(pricingRule.action(cart)).toBeGreaterThan(0);
+    expect(cart.vga.discount).toBe(2 * itemPrice);
+  }
+});
